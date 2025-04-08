416c8dbde85ddfc1edaabb546570dfc8ec4f1294
Maciej Leśniak <lesniakmaciek9040@gmail.com>
2025-04-08 13:00:17 +0200

    [FEAT]: Authentication Provider added.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,25 @@
-import './App.css';
-import { Route, Routes } from "react-router-dom";
-import PageLayout from './components/PageLayout';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PageLayout from "./components/PageLayout";
 import HomePage from "./pages/HomePage";
-
-
+import AuthProvider from "./context/AuthProvider";
 
 function App() {
-
   return (
-    <Routes>
-      <Route path="/" element={<PageLayout />}>
-        <Route index element={<HomePage />} />
-        <Route path="/wallet" element={<HomePage />} />
-        <Route path="/groups" element={<HomePage />} />
-        <Route path="/about" element={<HomePage />} />
-        <Route path="/account" element={<HomePage />} />
-      </Route>
-    </Routes>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<PageLayout />}>
+            <Route index element={<HomePage />} />
+            <Route path="/wallet" element={<HomePage />} />
+            <Route path="/groups" element={<HomePage />} />
+            <Route path="/about" element={<HomePage />} />
+            <Route path="/account" element={<HomePage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
-export default App
+export default App;
